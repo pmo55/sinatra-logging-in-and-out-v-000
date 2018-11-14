e8403aa4d8be6316f3f70c46de49c7083d7c1d1a
Parsa Moshiri <parsamoshiri5@gmail.com>
2018-11-14 20:35:46 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,6 +12,9 @@ class ApplicationController < Sinatra::Base
 
   post '/login' do
     @user = User.find_by(username: params[:username])
+    if @user != nil && @user.password == params[:password]
+      session[:user_id] = @user.id
+    end
   
   end
 
